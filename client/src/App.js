19e9ea95fe11cbd9dc43d7208458904e009fc54b
Sajib <contact2sajib@gmail.com>
2021-11-17 21:29:18 +0600

    context intialized

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,50 @@
-import React from 'react'
-import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import "./App.css";
 
-import Navigation from './components/Navigation/Navigation';
-import Auth from './pages/Auth'
-import Bookings from './pages/Bookings'
-import Events from './pages/Events'
+import Navigation from "./components/Navigation/Navigation";
+import Auth from "./pages/Auth";
+import Bookings from "./pages/Bookings";
+import Events from "./pages/Events";
+import AuthContext from "./context/auth-context";
 
 const App = () => {
+  const [token, setToken] = React.useState(null);
+  const [userId, setUserId] = React.useState(null);
+
+  const login = (token, userId, tokenExpiration) => {
+    setUserId(userId);
+    setToken(token);
+  };
+  console.log(token, userId);
+  const logout = () => {
+    setToken(null);
+    setUserId(null);
+  };
+
   return (
     <BrowserRouter>
-    <Navigation />
-    <React.Fragment>
-      <main className="main-content">
-    <Switch>
-      <Redirect from="/" to="/auth" exact />
-    <Route path="/auth">
-      <Auth />
-    </Route>
-    <Route path="/events" component={Events} />
-    <Route path="/bookings" component={Bookings} />
-    </Switch>
-    </main>
-    </React.Fragment>
+      <React.Fragment>
+        <AuthContext.Provider
+          value={{ userId: userId, token: token, login: login, logout: logout }}
+        >
+          <Navigation />
+          <main className="main-content">
+            <Switch>
+              {!token && <Redirect from="/" to="/auth" exact />}
+              {token && <Redirect from="/" to="/events" exact />}
+              {token && <Redirect from="/auth" to="/events" exact />}
+              <Route path="/auth">
+                <Auth />
+              </Route>
+              <Route path="/events" component={Events} />
+              {token && <Route path="/bookings" component={Bookings} />}
+            </Switch>
+          </main>
+        </AuthContext.Provider>
+      </React.Fragment>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,11 +1,103 @@
-import React from 'react'
+import React, { useContext } from "react";
+
+import "./Auth.css";
+
+import AuthContext from "../context/auth-context";
 
 const Auth = () => {
-    return (
-        <div>
-            The Auth Page
-        </div>
-    )
-}
+  const [isLoggedIn, setIsLoggedIn] = React.useState(true);
+  const { login } = useContext(AuthContext);
+  const emailRef = React.useRef();
+  const passwordRef = React.useRef();
 
-export default Auth
+  const swicthModeHandler = () => {
+    setIsLoggedIn((prevState) => {
+      return !prevState;
+    });
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+
+    if (email.trim().length === 0 || password.trim().length === 0) {
+      return;
+    }
+
+    let requestBody = {
+      query: `
+          query {
+            login(email: "${email}", password: "${password}") {
+              userId
+              token
+              tokenExpiration
+            }
+          }
+        `,
+    };
+
+    if (!isLoggedIn) {
+      requestBody = {
+        query: `
+            mutation {
+              createUser(userInput: {email: "${email}", password: "${password}"}) {
+                _id
+                email
+              }
+            }
+          `,
+      };
+    }
+
+    // API Call
+    fetch("http://localhost:4000/graphql", {
+      method: "POST",
+      body: JSON.stringify(requestBody),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => {
+        if (res.status !== 200 && res.status !== 201) {
+          throw new Error("Failed");
+        }
+        return res.json();
+      })
+      .then((resData) => {
+        console.log(resData);
+        if (resData.data.login.token) {
+          login(
+            resData.data.login.token,
+            resData.data.login.userId,
+            resData.data.login.tokenExpiration
+          );
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <form className="auth-form" onSubmit={submitHandler}>
+      <div className="from-controll">
+        <label htmlFor="email">E-mail</label>
+        <input type="email" id="email" ref={emailRef} />
+      </div>
+      <div className="from-controll">
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" ref={passwordRef} />
+      </div>
+      <div className="from-actions">
+        <button type="submit">Submit</button>
+        <button type="button" onClick={swicthModeHandler}>
+          Switch to {isLoggedIn ? "Signup" : "Login"}
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default Auth;
